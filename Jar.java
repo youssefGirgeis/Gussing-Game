2b3a0ef27dadc5026da1d50334119b448fd921eb
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-02-04 13:37:05 -0500

    Update Jar.java

--- Jar.java
+++ Jar.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
-public class Jar{
-
+public class Jar {
+	
 	private String itemName;
 	private int maxNumberItems;
 
@@ -11,33 +11,55 @@ public class Jar{
 		this.itemName = itemName;
 		this.maxNumberItems = maxNumberItems;
 	}
+	
+	public String getItemName(){
+		return itemName;
+	}
+	
+	public int getMaxNumberItems(){
+		return maxNumberItems;
+	}
 
 	public int fill(){
 
 		Random random = new Random();
-		int randomNumber = random.nextInt(maxNumberItems + 1);
+		int randomNumber = random.nextInt((maxNumberItems - 1) + 1) + 1;
+		//System.out.print(randomNumber);
 		return randomNumber; 
 	}
 
 	public void guessNumber(){
-
+		
+		Scanner scanner = new Scanner(System.in);
 		int randomNumber = fill();
 		int counter = 0;
-
-		Scanner scanner = new Scanner(System.in);
-
-
+		int guess;
 		
 		do{
-
 			System.out.print("Guess: ");
-			int guess = scanner.nextInt();
-			counter++;
+			guess = scanner.nextInt();
+			
+			if(guess > maxNumberItems){
+				System.out.printf("Your guess must be less than %d.%n", maxNumberItems);
+			}else{
+				counter++;
+				guessHighLow(guess, randomNumber, counter);
+			}
 
 		}while(guess != randomNumber);
 
-		System.out.println("Congratulations - you guessed that there %d %s in the jar! It took you %d guess(es) to get right.",
-			randomNumber, itemName, counter);
-
 	}
+	
+	public void guessHighLow(int guess, int randomNumber, int counter){
+		
+		if(guess > randomNumber){
+			System.out.println("Your guess is too high");
+		}else if(guess < randomNumber){
+			System.out.println("Your guess is too low");
+		}else{
+			System.out.printf("Congratulations - you guessed that there are %d %s in the jar! It took you %d guess(es) to get right.",
+					randomNumber, itemName, counter);
+		}	
+	}
+
 }

e59ac0993eba5b9ba1ade6e266da98a991d44b4e
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-02-04 13:36:06 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,9 +1,13 @@
+
 public class Game {
-    public static void main(String[] args) {
 
-    	Prompter prompter = new Prompter();
+	public static void main(String[] args) {
+		
+		Prompter prompter = new Prompter();
     	prompter.promptForAdminstrator();
+    	prompter.promptForPlayer();
 
+	}
 
-    }
 }
+

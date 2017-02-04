4b9bc75b3158bb24e025b6b2d95f44bb3a13a782
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-02-04 00:11:14 -0500

    add fields and constructor to Jar class

---
+++ Jar.java
@@ -0,0 +1,11 @@
+public class Jar{
+
+	private String itemName;
+	private int maxNumberItems;
+
+	
+	public Jar(String itemName, int maxNumberItems){
+		this.itemName = itemName;
+		this.maxNumberItems = maxNumberItems;
+	}
+}

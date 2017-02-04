eda6fbf1716c7c5ce49bb989b4b2c019516d4348
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-02-04 00:20:11 -0500

    create promptForQuestion method

---
+++ Prompter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Prompter{
+
+
+	public void promptForQuestion(){
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("ADMINISTRATOR SETUP\n====================");
+		System.out.print("Name of items in the Jar: ");
+
+		String itemName = scanner.nextLine();
+
+		System.out.print("Maximum number of %s in the jar: ", itemName);
+		int maxNumber = scanner.nextInt();
+
+		Jar jar = new Jar(itemName, maxNumber);
+	} 
+}

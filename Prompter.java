a4085e0da905d831885f9c05cdb087bf058a62d0
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-02-04 00:38:31 -0500

    update prompt method

--- Prompter.java
+++ Prompter.java
@@ -16,5 +16,8 @@ public class Prompter{
 
 		Jar jar = new Jar(itemName, maxNumber);
 
+		System.out.printf("Your goal is to guess how many %s are in the jar. Your Guess shoud be between 1 and %d",
+			itemName, maxNumber);
+
 	}
 }
